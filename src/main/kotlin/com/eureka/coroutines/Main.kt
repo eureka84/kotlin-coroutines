38f01b2e4445667f5bec3b8deece77dbe936c2c6
eureka84 <angelosciarra@ymail.com>
2019-11-10 13:03:39 +0100

    first step: launch is a suspended function so gets executed after run

--- src/main/kotlin/com/eureka/coroutines/Main.kt
+++ src/main/kotlin/com/eureka/coroutines/Main.kt
@@ -1,6 +1,25 @@
 package com.eureka.coroutines
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    testRunFunction()
 }
 
+fun testRunFunction() {
+
+    GlobalScope.launch {
+        println("In start : ${getThreadName()}")
+        Thread.sleep(200)
+        println("In ended : ${getThreadName()}")
+    }
+
+    run {
+        println("Out start: ${getThreadName()}")
+        Thread.sleep(300)
+        println("Out ended: ${getThreadName()}")
+    }
+}
+
+fun getThreadName(): String = Thread.currentThread().name
