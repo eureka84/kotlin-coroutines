3582bea28cd22efd19738b5bf6e529874cf97b99
eureka84 <angelosciarra@ymail.com>
2020-04-26 20:35:25 +0200

    ComposingCoroutines.kt

---
+++ src/main/kotlin/com/eureka/coroutines/ComposingCoroutines.kt
@@ -0,0 +1,41 @@
+package com.eureka.coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+
+fun main() = runBlocking {
+//    sequential()
+    concurrent()
+}
+
+suspend fun sequential(){
+    val time = measureTimeMillis {
+        val one = doSomethingUsefulOne()
+        val two = doSomethingUsefulTwo()
+        println("The answer is ${one + two}")
+    }
+    println("Completed in $time ms")
+}
+
+suspend fun CoroutineScope.concurrent(){
+    val time = measureTimeMillis {
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $time ms")
+}
+
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
+}
