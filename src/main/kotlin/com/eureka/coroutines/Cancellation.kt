03a74cf4498f6cb740b73f840251dfd48d840346
eureka84 <angelosciarra@ymail.com>
2020-04-26 20:05:15 +0200

    Cancellation

---
+++ src/main/kotlin/com/eureka/coroutines/Cancellation.kt
@@ -0,0 +1,48 @@
+package com.eureka.coroutines
+
+import kotlinx.coroutines.*
+
+
+fun main() = runBlocking {
+    unstoppableCoroutine()
+//    stoppableCoroutine()
+}
+
+private suspend fun CoroutineScope.stoppableCoroutine() {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (i < 5) { // cancellable computation loop
+            yield()
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("job: I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
+
+private suspend fun CoroutineScope.unstoppableCoroutine() {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (i < 5) { // cancellable computation loop
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("job: I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
